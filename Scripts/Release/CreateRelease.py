84d82f97b6ad0057c3b08d532406777de3cb13e5
Josh Rideout <joshrideout88@gmail.com>
2021-03-29 21:14:25 +0100

    testing

--- Scripts/Release/CreateRelease.py
+++ Scripts/Release/CreateRelease.py
@@ -30,7 +30,7 @@ def create_release_branch(platform, version):
     run("git commit -am \"Creating release branch for version: %s\"" % (version))
     run("git push --set-upstream origin %s" % (branch_name))
 
-def does_release_branch_exist(platform, branch_name):
+def does_release_branch_exist(platform, version):
     branch_name = release_branch_name(platform, version)
     git_result = result("git ls-remote --exit-code . origin/%s" % (branch_name))
     if branch_name in git_result:
@@ -52,7 +52,7 @@ if branch == "develop":
     platform = get_platform()
     project_version = project_version_number(platform)
 
-    if does_release_branch_exist() is True:
+    if does_release_branch_exist(platform, project_version) is True:
         print("Cannot create release branch. The %s release branch for version %s already exists" % (platform, project_version))
         sys.exit()
 
